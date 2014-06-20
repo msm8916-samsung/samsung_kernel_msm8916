148629506ed0597b180c1ff7ea6e2a722edb7a60
Iliya Varadzhakov <ivarad@codeaurora.org>
2014-06-19 20:03:00 -0700

    msm: cpp: Validate frame message before manipulating it
    CPP frame message is used to send all frame data
    to Microcontroller. It is sent every frame. CPP kernel
    driver has to add information to it before transfer it.
    The message has to be validated before manipulations.
    If it is not valid the message and corresponding frame
    are discarded.
    
    Change-Id: Id272eb2296233c66befd015f41f19a9fbc551572
    Signed-off-by: Iliya Varadzhakov <ivarad@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -1406,6 +1406,18 @@ static int msm_cpp_cfg_frame(struct cpp_device *cpp_dev,
 	}
 	cpp_frame_msg = new_frame->cpp_cmd_msg;
 
+	if (cpp_frame_msg == NULL ||
+		(new_frame->msg_len < MSM_CPP_MIN_FRAME_LENGTH)) {
+		pr_err("%s %d Length is not correct or frame message is missing\n",
+			__func__, __LINE__);
+		return -EINVAL;
+	}
+
+	if (cpp_frame_msg[new_frame->msg_len - 1] != MSM_CPP_MSG_ID_TRAILER) {
+		pr_err("%s %d Invalid frame message\n", __func__, __LINE__);
+		return -EINVAL;
+	}
+
 	in_phyaddr = msm_cpp_fetch_buffer_info(cpp_dev,
 		&new_frame->input_buffer_info,
 		((new_frame->identity >> 16) & 0xFFFF),
@@ -1505,6 +1517,12 @@ static int msm_cpp_cfg_frame(struct cpp_device *cpp_dev,
 =======
 		goto phyaddr_err;
 >>>>>>> 150a5a83ade (msm: camera: Add compat ioctl changes for cpp)
+	}
+
+	if ((stripe_base + num_stripes*27 + 1) != new_frame->msg_len) {
+		pr_err("Invalid frame message\n");
+		rc = -EINVAL;
+		goto phyaddr_err;
 	}
 
 	for (i = 0; i < num_stripes; i++) {
