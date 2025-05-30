67367eff7bbbb8c1804e18d4cedf6e325aa444be
Your Name <you@example.com>
2025-05-30 11:23:45 +0200

    unify kernel build scripts arm/arm64

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 mkdir output
 
--- build_kernel_64.sh
+++ build_kernel_64.sh
@@ -7,5 +7,5 @@ mkdir output
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
-cp output/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+cp output/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/zImage
 
